185608a5a9b44fcf84aedb5a264d8a36f7ea2357
Maxim Pinyaguin <88331457+Ozurexus@users.noreply.github.com>
2022-10-03 16:50:35 +0300

    Deleting obsolete files

--- week5/ex3.sh
+++
@@ -1,5 +0,0 @@
-gcc -pthread ex3.c -o ex && (time ./ex 10000000 1) 2> ex3.txt
-gcc -pthread ex3.c -o ex && (time ./ex 10000000 2) 2>> ex3.txt
-gcc -pthread ex3.c -o ex && (time ./ex 10000000 4) 2>> ex3.txt
-gcc -pthread ex3.c -o ex && (time ./ex 10000000 10) 2>> ex3.txt
-gcc -pthread ex3.c -o ex && (time ./ex 10000000 100) 2>> ex3.txt
