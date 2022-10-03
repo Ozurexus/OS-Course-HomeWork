c1e39b1d7591558574787e31eebe2e7105a61afa
Maxim Pinyaguin <88331457+Ozurexus@users.noreply.github.com>
2022-10-03 16:51:05 +0300

    Deleting obsolete files

--- week5/ex4.sh
+++
@@ -1,5 +0,0 @@
-gcc -pthread ex4.c -o ex && (time ./ex 10000000 1) 2> ex4.txt
-gcc -pthread ex4.c -o ex && (time ./ex 10000000 2) 2>> ex4.txt
-gcc -pthread ex4.c -o ex && (time ./ex 10000000 4) 2>> ex4.txt
-gcc -pthread ex4.c -o ex && (time ./ex 10000000 10) 2>> ex4.txt
-gcc -pthread ex4.c -o ex && (time ./ex 10000000 100) 2>> ex4.txt
